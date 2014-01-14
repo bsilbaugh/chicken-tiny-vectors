6b562022eca795122b80381cfa925e66e9558ec2
Benjamin Silbaugh <ben.silbaugh@gmail.com>
2014-01-14 15:03:23 -0500

    Added test suite

---
+++ tests/run.scm
@@ -0,0 +1,72 @@
+;;;
+;;; Tiny Vectors Test Suite
+;;;
+
+(use test)
+(use tiny-vectors)
+
+(test-group "element access"
+  (test 0.1 (vec-elem-1 (make-vec 0.1 0.2 0.3)))
+  (test 0.2 (vec-elem-2 (make-vec 0.1 0.2 0.3)))
+  (test 0.3 (vec-elem-3 (make-vec 0.1 0.2 0.3))))
+
+(test-group "algebraic operations +, -, *, /"
+  (test-assert (equal? (make-vec 1.1 2.2 3.3)
+					   (vec+vec (make-vec 0.1 0.2 0.3)
+								(make-vec 1.0 2.0 3.0))))
+  (test-assert (equal? (make-vec 0.9 2.1 -0.3)
+					   (vec-vec (make-vec 1.0 2.0 0.0)
+								(make-vec 0.1 -0.1 0.3)))))
+
+(test-group "dot product"
+  (test  1.0  (dot (make-vec  1.0  0.0  0.0) (make-vec  1.0  0.0  0.0)))
+  (test  0.0  (dot (make-vec  1.0  0.0  0.0) (make-vec  0.0  1.0  0.0)))
+  (test  0.0  (dot (make-vec  1.0  0.0  0.0) (make-vec  0.0  0.0  1.0)))
+  (test  0.0  (dot (make-vec  0.0  1.0  0.0) (make-vec  1.0  0.0  0.0)))
+  (test  1.0  (dot (make-vec  0.0  1.0  0.0) (make-vec  0.0  1.0  0.0)))
+  (test  0.0  (dot (make-vec  0.0  1.0  0.0) (make-vec  0.0  0.0  1.0)))
+  (test  0.0  (dot (make-vec  0.0  0.0  1.0) (make-vec  1.0  0.0  0.0)))
+  (test  0.0  (dot (make-vec  0.0  0.0  1.0) (make-vec  0.0  1.0  0.0)))
+  (test  1.0  (dot (make-vec  0.0  0.0  1.0) (make-vec  0.0  0.0  1.0)))
+  (test -1.0  (dot (make-vec -1.0  0.0  0.0) (make-vec  1.0  0.0  0.0)))
+  (test  0.0  (dot (make-vec  0.0  0.0  0.0) (make-vec  1.0  0.0  0.0))))
+
+(test-group "cross product"
+  (test-assert (equal? (make-vec 0.0 0.0 0.0) 
+					   (cross (make-vec 1.0 0.0 0.0) 
+							  (make-vec 1.0 0.0 0.0))))
+  (test-assert (equal? (make-vec 0.0 0.0 1.0) 
+					   (cross (make-vec 1.0 0.0 0.0) 
+							  (make-vec 0.0 1.0 0.0))))
+  (test-assert (equal? (make-vec 0.0 -1.0 0.0) 
+					   (cross (make-vec 1.0 0.0 0.0) 
+							  (make-vec 0.0 0.0 1.0))))
+  (test-assert (equal? (make-vec 0.0 0.0 -1.0) 
+					   (cross (make-vec 0.0 1.0 0.0) 
+							  (make-vec 1.0 0.0 0.0))))
+  (test-assert (equal? (make-vec 0.0 0.0 0.0) 
+					   (cross (make-vec 0.0 1.0 0.0) 
+							  (make-vec 0.0 1.0 0.0))))
+  (test-assert (equal? (make-vec 1.0 0.0 0.0) 
+					   (cross (make-vec 0.0 1.0 0.0) 
+							  (make-vec 0.0 0.0 1.0))))
+  (test-assert (equal? (make-vec 0.0 1.0 0.0) 
+					   (cross (make-vec 0.0 0.0 1.0) 
+							  (make-vec 1.0 0.0 0.0))))
+  (test-assert (equal? (make-vec -1.0 0.0 0.0) 
+					   (cross (make-vec 0.0 0.0 1.0) 
+							  (make-vec 0.0 1.0 0.0))))
+  (test-assert (equal? (make-vec 0.0 0.0 0.0) 
+					   (cross (make-vec 0.0 0.0 1.0) 
+							  (make-vec 0.0 0.0 1.0)))))
+
+(test-group "mag"
+  (test 0.0 (mag (make-vec  0.0  0.0  0.0)))
+  (test 1.0 (mag (make-vec  1.0  0.0  0.0)))
+  (test 1.0 (mag (make-vec  0.0  1.0  0.0)))
+  (test 1.0 (mag (make-vec  0.0  0.0  1.0)))
+  (test 1.0 (mag (make-vec -1.0  0.0  0.0)))
+  (test 1.0 (mag (make-vec  0.0 -1.0  0.0)))
+  (test 1.0 (mag (make-vec  0.0  0.0 -1.0)))
+  (test (sqrt 0.14) (mag (make-vec 0.1 -0.2 0.3))))
+
